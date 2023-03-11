a981972a502ad9d4cc02f0d809829b9d389dfa41
roman90x <roman90x@icloud.com>
2023-03-12 01:55:58 +0600

    finished hw_3.5

--- src/main/java/me/roman/recipesapp/controllers/IngredientController.java
+++ src/main/java/me/roman/recipesapp/controllers/IngredientController.java
@@ -1,5 +1,7 @@
 package me.roman.recipesapp.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import me.roman.recipesapp.model.Ingredient;
 import me.roman.recipesapp.service.IngredientService;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/ingredient")
+@Tag(name = "API по работе с ингредиентами", description = "CRUD - операции")
 public class IngredientController {
     public final IngredientService ingredientService;
 
@@ -16,23 +19,44 @@ public class IngredientController {
         this.ingredientService = ingredientService;
     }
 
+    @Operation(
+            summary = "Добавление ингредиента"
+    )
     @PostMapping
     public ResponseEntity<Ingredient> save(@RequestBody Ingredient ingredient) {
         return ResponseEntity.ok(ingredientService.save(ingredient));
     }
 
+    @Operation(
+            summary = "Получение информации об ингредиенте по ID"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<Ingredient> getById(@PathVariable Long id) {
         return ResponseEntity.of(ingredientService.getById(id));
     }
 
+    @Operation(
+            summary = "Редактирование ингредиента по ID"
+    )
     @PutMapping("/{id}")
     public ResponseEntity<Ingredient> update(@PathVariable Long id, @RequestBody Ingredient ingredient) {
         return ResponseEntity.ok(ingredientService.update(id, ingredient));
     }
 
+    @Operation(
+            summary = "Удаление ингредиента по ID"
+    )
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Ingredient> delete(@PathVariable Long id) {
+        return ResponseEntity.ok(ingredientService.delete(id));
+
+    }
+
+    @Operation(
+            summary = "Получение полного списка ингредиентов"
+    )
     @GetMapping()
-    public ResponseEntity<Map<Long, Ingredient>> getAll(){
+    public ResponseEntity<Map<Long, Ingredient>> getAll() {
         return ResponseEntity.ok(ingredientService.getAll());
     }
 
--- src/main/java/me/roman/recipesapp/model/Recipe.java
+++ src/main/java/me/roman/recipesapp/model/Recipe.java
@@ -1,74 +1,17 @@
 package me.roman.recipesapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.util.List;
 import java.util.Objects;
 
+@Data
+@AllArgsConstructor
 public class Recipe {
     private String name;
     private int cookingTime;
     private List<Ingredient> ingredients;
     private List<String> steps;
-
-    public Recipe(String name, int cookingTime, List<Ingredient> ingredients, List<String> steps) {
-        this.name = name;
-        this.cookingTime = cookingTime;
-        this.ingredients = ingredients;
-        this.steps = steps;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getCookingTime() {
-        return cookingTime;
-    }
-
-    public void setCookingTime(int cookingTime) {
-        this.cookingTime = cookingTime;
-    }
-
-    public List<Ingredient> getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(List<Ingredient> ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public List<String> getSteps() {
-        return steps;
-    }
-
-    public void setSteps(List<String> steps) {
-        this.steps = steps;
-    }
-
-    @Override
-    public String toString() {
-        return "Recipe{" +
-                "name='" + name + '\'' +
-                ", cookingTime=" + cookingTime +
-                ", ingredients=" + ingredients +
-                ", steps=" + steps +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Recipe recipe = (Recipe) o;
-        return cookingTime == recipe.cookingTime && name.equals(recipe.name) && ingredients.equals(recipe.ingredients) && steps.equals(recipe.steps);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, cookingTime, ingredients, steps);
-    }
 }
 
